3d2386739afafa95a6b472f5d0f85e679bfdd5dc
RealTkwk <nachofreire@gmail.com>
2015-12-17 14:08:27 -0300

    PtepMeals changes

--- PrepMeals.py
+++ PrepMeals.py
@@ -9,15 +9,25 @@ secondary_seasonings = ['Chia Seeds', 'Sunflower Seeds', 'Quinoa Seeds', 'Almond
 
 mls = re.compile(r'<meals (?P<meals>\d{1,2})>', re.I | re.M)
 
-text = '<meals 12>'
+text = '<meals 5>'
 
 meals_qty = mls.findall(text)
 
+ingredients = []
+
 for i in range(int(meals_qty[0])):
+
     main = random.sample(protein, 1)
     side = random.sample(sides, 2)
     main_season = random.sample(main_seasoning, 1)
     sec_season = random.sample(secondary_seasonings, 1)
+    print(main)
+    ingredients.extend(main+side+main_season+sec_season)
 
-    print('-Recipe number {}-\n{}\n{}, {}\n{}, {} and Salt\n'.format(i+1, main[0], side[0], side[1], main_season[0],
-                                                                     sec_season[0]))
+    #print('-Recipe number {}-\n{}\n{}, {}\n{}, {} and Salt\n'.format(i+1, main[0], side[0], side[1], main_season[0],
+    #                                                                 sec_season[0]))
+
+print(ingredients)
+
+def grocery_list():
+ingredients
