b74ed9a6c980b6ed842f5963d19483281646240d
RealTkwk <nachofreire@gmail.com>
2015-12-21 10:37:03 -0300

    Fixed <status> again.

--- Emma.py
+++ Emma.py
@@ -123,8 +123,8 @@ if __name__ == '__main__':
 
             if sStatus:
                 message.append('{} runs so far. That\'s {} days, {} hours or {} minutes. Real process time {}s'
-                               .format(runs, ((runs*2)//1440) + totTime//1440, ((runs*2)//60) + totTime//60, runs*2,
-                                       int(totTime)))
+                               .format(runs, ((runs*2)//1440) + totTime//86400, ((runs*2)//60) + totTime//3600,
+                                       runs*2 + totTime//60, int(totTime)))
 
             if sAlive:
                 message.append('Yes, I\'m alive! :)')
