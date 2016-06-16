5649bd9d9fbf8d5200d95f5b91c3cfd4ef861add
RealTkwk <nachofreire@gmail.com>
2016-06-16 08:43:20 -0300

    Fixed phantomjs driver location

--- Emma.py
+++ Emma.py
@@ -50,7 +50,7 @@ sig = re.compile(r'<sig (?P<month>1[0-2]|[1-9])>', re.I | re.M)
 # Google Keep Note initialization
 log('Creating Messenger objects.')
 chromedriver = "/home/Projects/Project-E.M.M.A./chromedriver"
-phantomjs = "/home/Projects/Project-E.M.M.A./chromedriver"
+phantomjs = "/home/Projects/Project-E.M.M.A./phantomjs"
 keep = GoogleKeep(account, password, note, backaccount, verbose='yes', location=phantomjs)
 grocery = GoogleKeep(account, password, grocery_note, backaccount, verbose='yes', location=phantomjs)
 recipes = GoogleKeep(account, password, recipes_note, backaccount, verbose='yes', location=phantomjs)
